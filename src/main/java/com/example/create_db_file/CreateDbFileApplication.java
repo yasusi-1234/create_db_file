7109f368251a0083f5a2f8c9f1cf7c97ca37426c
yasu <yasusi0117@live.jp>
2021-09-20 22:49:50 +0900

    Initial commit

---
+++ src/main/java/com/example/create_db_file/CreateDbFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.create_db_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateDbFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateDbFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/create_db_file/CreateDbFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.create_db_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateDbFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
