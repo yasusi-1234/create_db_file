e8bf3ffdd12448d4b4fa807c361adf01369c80ac
yasu <yasusi0117@live.jp>
2021-11-23 17:58:06 +0900

    バリデーションクラスの作成

---
+++ src/main/java/com/example/create_db_file/controller/form/parts/validator/TemporalConfirm.java
@@ -0,0 +1,39 @@
+package com.example.create_db_file.controller.form.parts.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+import java.time.temporal.Temporal;
+
+@Documented
+@Constraint(validatedBy = { TemporalConfirmValidator.class })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TemporalConfirm {
+
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Field name
+     */
+    String field() default "";
+
+    String minName() default "minTime";
+
+    String maxName() default  "maxTime";
+
+//    /**
+//     * Class
+//     */
+//    Class<? extends Temporal> classType() default Temporal.class;
+
+    @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    public @interface List {
+        NumberConfirm[] value();
+    }
+}
