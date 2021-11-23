70fec36891c54d2ae447b23be7b1124abf9abd90
yasu <yasusi0117@live.jp>
2021-11-23 17:58:33 +0900

    コンフィグクラスの追加

---
+++ src/main/java/com/example/create_db_file/config/BeanConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.create_db_file.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
